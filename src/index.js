eecaa525d9c9e6de6dfbfcce0230b5a7ecd34424
Lingg <72445765+NQL4901@users.noreply.github.com>
2022-07-27 23:04:39 +0700

    GlobalStyles

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import GlobalStyles from '~/components/GlobalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <React.StrictMode>
+    // GlobalStyles
     <GlobalStyles>
         <App />
     </GlobalStyles>,
